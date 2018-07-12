ce85f9c7f09e595ffded955b6119199840b46816
Cesar Luna <cesarlunalimon@gmail.com>
2018-07-11 21:29:27 -0500

    create user&render

--- public/script/app.js
+++ public/script/app.js
@@ -1,13 +1,21 @@
+var create = document.querySelector('form');
+var _users;
+
 fetch('http://localhost:8080/api/users')
 .then(response =>{
     if(!response.ok){
-        return Promise.reject(respomse.json());
+        return Promise.reject(response.json());
     }
     return response.json();
 })
 .then(users => {
     console.log(users);
-    var userTable = users.map(user => {
+    _users = users;
+    render();
+}).catch(error => console.log(error));
+
+function render(){
+    var userTable = _users.map(user => {
         return `<tr>
         <td>${user.nombre}</td>
         <td>${user.apellido1}</td>
@@ -20,8 +28,39 @@ fetch('http://localhost:8080/api/users')
     <th>Apellido paterno</th>
     <th>Apellido materno</th>
     <th>Correo</th>
-</tr>`);
+    </tr>`);
     var tabla = document.querySelector('#tabla');
     tabla.innerHTML = userTable.join('');
+}
+
+create.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    var nombre = create.nombre.value;
+    var apellido1 = create.apellido1.value;
+    var apellido2 = create.apellido2.value;
+    var email = create.email.value;
+    var password = create.password.value;
+
+    fetch('http://localhost:8080/api/users', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({nombre, apellido1, apellido2, email, password})
+    })
+    .then(response =>{
+        if(!response.ok){
+            return Promise.reject(response.json());
+        }
+        return response.json();
+    })
+    .then(user => {
+        _users.push(user);
+        render();
+        create.reset();
+    })
+    .catch(error => console.log(error));
 })
-.catch(error => console.log(error));
+
