e9ff7bdf6f7005641a981bec8adccd61277d76bb
Cesar Luna <cesarlunalimon@gmail.com>
2018-07-16 19:17:24 -0500

    terminado

--- public/script/app.js
+++ public/script/app.js
@@ -21,6 +21,7 @@ function render(){
         <td>${user.apellido1}</td>
         <td>${user.apellido2}</td>
         <td>${user.email}</td>
+        <td><button data-userid=${user._id}>Borrar</button></td>
         </tr>`
     })
     userTable.unshift(`<tr>
@@ -28,10 +29,37 @@ function render(){
     <th>Apellido paterno</th>
     <th>Apellido materno</th>
     <th>Correo</th>
+    <th></th>
     </tr>`);
     var tabla = document.querySelector('#tabla');
     tabla.innerHTML = userTable.join('');
 }
+
+const tabla = document.querySelector('#tabla');
+tabla.addEventListener('click', function(e){
+    e.preventDefault();
+    //console.log('Click', e.currentTarget, e.target, e.target.innerHTML);
+    if(e.target.innerHTML.trim() === "Borrar"){
+        //console.log('Este es el bueno');
+        //console.log(e.target.dataset.userid);
+
+        var uid = e.target.dataset.userid;
+        
+        fetch(`http://localhost:8080/api/users/${uid}`,{
+            method: 'DELETE',
+            headers: {
+            'Accept': 'application/json',
+            'Content-Type':'application/json'
+        }
+    }).then(response =>{
+        if(!response.ok){
+            return Promise.reject(response.json());
+        }
+       _users = _users.filter(user => user._id !== uid);
+        render();
+    }).catch(error => console.log(error));
+}
+});
 
 create.addEventListener('submit', function(e){
     e.preventDefault();
@@ -63,4 +91,3 @@ create.addEventListener('submit', function(e){
     })
     .catch(error => console.log(error));
 })
-
